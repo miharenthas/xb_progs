96ab4e506d23d78893b31f2a3706b0049443f052
Lorenzo Zanetti <lzanetti@ikp.tu-darmstadt.de>
2017-03-28 11:36:50 +0200

    xb_data_cut_on_field/nrg: updated the routines so they actually work (they did not) and remove empty events after cut. Tested: all works ok.

--- octave/msrc/xb_data_cut_on_nrg.m
+++ octave/msrc/xb_data_cut_on_nrg.m
@@ -7,25 +7,74 @@
 %       NOTE: those for which TRUE is returned are KEPT!
 
 function [evt, nb_removed] = xb_data_cut_on_nrg( evt, op_handle )
-	%klz is a tructure representing the event
-	%this will be maintained
+	%first, check on the package
+	try
+		pkg load parallel;
+	catch
+		error( 'parallel package not installed!' );
+	end
 	
 	if ~is_function_handle( op_handle )
 		error( "Second argument **MUST** be a function handle!" );
 	end
+	if isempty( evt )
+		nb_removed = 0;
+		return;
+	end
 	
+	%prepare to spli the events in the number of processes
+	nb_events = length( evt );
+	adj = mod( nb_events, nproc );
+	if adj adj = nproc - adj; end
+	idx_part = 1:(nb_events+adj)/nproc:(nb_events+adj);
+	idx_part = linspace( idx_part, ...
+	                     idx_part+(nb_events+adj)/nproc-1, ...
+	                     (nb_events+adj)/nproc );
+	
+	%split the events into a cell
+	evt_part = {};
+	evt_rest = [];
+	nbr_rest = 0;
+	nb_proc = 1;
+	for ii=1:nproc
+		try
+			evt_part(ii) = evt(idx_part(ii,:));
+		catch
+			%I'm not yet sure this is the most brilliant solution
+			%but it's the best I can think right now.
+			[evt_rest nbr_rest] = xb_data_cut_on_field( evt(idx_part(ii):end), ...
+			                                            op_handle );
+			break;
+		end
+		nb_proc += 1;
+	end
+
+	%do the parallel execution
+	proc_handle = @( p ) _processor( p, op_handle, field_name );
+	[evt_part, nb_removed_part] = parcellfun( nb_proc, proc_handle, evt_part, 'VerboseLevel', 0 );
+	
+	%stitch together the stuff
+	evt = reshape( evt_part, [], 1 );
+	if ~isempty( evt_rest ) evt = [evt, evt_rest]; end
+	nb_removed = sum( nb_removed_part ) + nbr_rest;
+	
+end
+
+%processor function
+function [evt, nb_removed] = _processor( evt, op_handle )
 	%loop clear the uninsteresting stuff.
 	nb_removed = sum( [evt.n] );
 	for ii=1:length( evt )
-		[keep_idx, ~] = find( op_handle( [evt(ii).e](:) ) );
-		if evt(ii).i; evt(ii).i = evt(ii).i( keep_idx ); end
-		if evt(ii).e; evt(ii).e = evt(ii).e( keep_idx ); end
-		if evt(ii).he; evt(ii).he = evt(ii).he( keep_idx ); end
-		if evt(ii).t; evt(ii).t = evt(ii).t( keep_idx ); end
-		if evt(ii).pt; evt(ii).pt = evt(ii).pt( keep_idx ); end
+		keep_idx = find( op_handle( [evt(ii).e](:) ) );
+		if size( evt(ii).i ) evt(ii).i = evt(ii).i( keep_idx ); end
+		if size( evt(ii).e ) evt(ii).e = evt(ii).e( keep_idx ); end
+		if size( evt(ii).he ) evt(ii).he = evt(ii).he( keep_idx ); end
+		if size( evt(ii).t ) evt(ii).t = evt(ii).t( keep_idx ); end
+		if size( evt(ii).pt ) evt(ii).pt = evt(ii).pt( keep_idx ); end
 		
 		%at the end of things, multiplicity update
 		evt(ii).n = length( keep_idx );
+		evt(ii).sum_e = sum( [evt(ii).e] );
 	end
 
 	nb_removed = nb_removed - sum( [evt.n] );
