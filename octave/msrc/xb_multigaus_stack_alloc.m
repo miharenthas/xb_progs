c9f369612a833cd315edd017986483595147d923
Lorenzo Zanetti <lzanetti@ikp.tu-darmstadt.de>
2017-04-09 11:12:33 +0200

    xb_roi_eval: corrected estimation of rois in the middle of the range. Works. xb_multigaus_stack_alloc: now it allocates with sigma and not 2*sigma. All tested: works.

--- octave/msrc/xb_multigaus_stack_alloc.m
+++ octave/msrc/xb_multigaus_stack_alloc.m
@@ -7,7 +7,7 @@ function gaus_stack = xb_multigaus_stack_alloc( data_abscissa, nb_gaus )
 	%loop create the gaussians
 	gaus_stack = {};
 	for gg=1:nb_gaus
-		gaus = @( p ) p(1)*exp( -(data_abscissa-p(2)).^2/p(3)^2 );
+		gaus = @( p ) p(1)*exp( -(data_abscissa-p(2)).^2/(2*p(3)^2) );
 		gaus_stack(gg) = gaus;
 	end
 end
