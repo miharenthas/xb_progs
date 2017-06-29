a90b396b93cef7fd7705d76f9f1ba1b7ada686b9
Lorenzo Zanetti <lzanetti@ikp.tu-darmstadt.de>
2017-06-30 08:36:39 +0900

    do_calib and cc_print update (leftover commit)

---
+++ octave/scripts/cryscalib/cc_print.m
@@ -0,0 +1,35 @@
+%this function prints the calibration data into a hooman readable file
+%
+% cc_print( file{_name}, writin_mode, c_pees, cp_err, dE_E )
+%
+% -- file{_name}: either a file pointer or a file name, the output's target
+% -- writing_mode: either "w", for overwrite, or "a", for append
+%                  (mute if the file is already opened).
+% -- crystal_number: the number of the crystal
+% -- c_pees: calibration parameters (come out of cc_do_calib)
+% -- cp_err: calibration parameters errorZ.
+% -- dE_E: the energy resolution (for each peak, the user will decide what to do)
+%returns nothing.
+%outputs a file.
+
+function cc_print( file, writing_mode, crystal_number, c_pees, cp_err, dE_E )
+	if ischar( file )
+		file = fopen( file, writing_mode );
+	end
+	
+	fprintf( file, 'Crystal number %d\n', crystal_number );
+	
+	fprintf( file, 'Calibration' );
+	fprintf( file, ' %lf', c_pees );
+	fprintf( file, '\n' );
+	
+	fprintf( file, 'Errors' );
+	fprintf( file, ' %lf', cp_err );
+	fprintf( file, '\n' );
+	
+	fprintf( file, 'dE_E' )
+	fprintf( file, ' %lf', dE_E );
+	fprintf( file, '\n' );
+	
+	fprintf( file, '\n######\n\n' );
+end
