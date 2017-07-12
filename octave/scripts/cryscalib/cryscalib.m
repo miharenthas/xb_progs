a123c366990e1ac5fb0440936761a20d7f7c4979
Lorenzo Zanetti <lzanetti@ikp.tu-darmstadt.de>
2017-07-12 17:05:48 +0200

    cryscalilb: written a bit.

--- octave/scripts/cryscalib/cryscalib.m
+++ octave/scripts/cryscalib/cryscalib.m
@@ -4,8 +4,45 @@
 %pre loaded and in raw form --preloaded needed for Mac).
 %
 % cryscalib
-% cryscalib( run_file_name, out_file_name_root )
+% cryscalib( run_file_list, source_profile_list )
 
 function cryscalib( varargin )
-	%...
+	%plan:
+	%0) parse files/input
+	%   --determine how many calibration runs,
+	%     how many sources (and where they are)
+	%1) Do the calibration for every (half of the CB)
+	%   run provided and for every crystal:
+	%   --cc_do_spectrum
+	%   --cc_do_cutoff
+	%   --cc_do_fitting
+	%   --cc_do_calib
+	%2) Combine the various calibration information of the
+	%   various runs (thus reducing the relative error?)
+	%3) --cc_print
+	
+	%make the settings for the program
+	%NOTE: probably, more stuff is gonna be into it
+	cc_settings = struct( 'rf_list', {}, 'sp_list', {} );
+	
+	if ~numel( varargin )
+		%use a prompt
+	elseif numel( varargin ) == 2
+		rf_list = varargin{1};
+		sp_list = varargin{2};
+		
+		%check in bulk
+		ck_str = @( p ) if ~ischar( p ) error( 'Not a string' ); end;
+		cellfun( ck_str, rf_list );
+		cellfun( ck_str, sp_list );
+		
+		%save it into cc_settings.
+		cc_settings.rf_list = rf_list;
+		cc_settings.sp_list = sp_list;
+	else
+		error( 'Inconsistent number of argument. Require two cells of strings.' );
+	end
+	
+	%open the files and
+	
 end
