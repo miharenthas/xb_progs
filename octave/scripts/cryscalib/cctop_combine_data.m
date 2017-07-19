75ef65e82c1d6f4466d2ef7810754ce3374dd28a
Lorenzo Zanetti <lzanetti@ikp.tu-darmstadt.de>
2017-07-19 16:18:27 +0200

    cryscalib: structural changes, now I do the calibration on the full given data set (multiple source, multiple runs). Writing finished, test pending.

--- octave/scripts/cryscalib/cctop_combine_data.m
+++ octave/scripts/cryscalib/cctop_combine_data.m
@@ -1,44 +1,15 @@
-%This tool alligns the data to the source profiles (they are supposed to be
-%in the SAME order) and merges the runs with the same source profile
+%This tool alligns merges the data and the source profiles.
 %So be careful to feed the software with mergeable data!
 %
-% [data, source_profiles, nb_runZ] = cctop_combine_data( data, source_profile )
+% [data, source_profiles] = cctop_combine_data( data, source_profile )
 
-function [data, source_profiles, nb_runZ] = cctop_combine_data( data, source_profile )
-	if numel( source_profile ) ~= numel( data )
-		error( 'different number of runs and source profiles.' );
-	end
+function [data, source_profiles] = cctop_combine_data( data, source_profile )
+	%merge the source profiles
+	source_profile = cell2mat( source_profile );
+	[~, ii] = sort( source_profile(1,:) );
+	source_profile = source_profile(:,ii);
 
-	%returns TRUE when the vectors are equal, FALSE otherwiese
-	%or an error if they have different shapes.
-	__ck_vect = @( p, q ) isempty( find( p ~= q ) ); %p and q are matrices of the same size
-	
-	%do some renaming
-	r_data = data;
-	r_sp = source_profile;
-	clear data, source_porfile; %data might be huge, cleanup.
-	data = {};
-	source_profle = {};
-	while ~isempty( source_profile )
-		__ck_this = @( p ) __ck_vec( p, r_sp{1} ); %make a check for this sp
-		idx = cellfun( __ck_this, srouce_profile ); %check it on all of them
-		
-		%move about data:
-		%save for this elaboration
-		d_buf = r_data( idx );
-		%and for the next
-		r_data = r_data( ~idx );
-		r_sp = r_sp( ~idx );
-		
-		%cat the data
-		data( numel( data )+1 ) = [];
-		for ii=1:numel( d_buf )
-			data(end) = [data{end}(:); d_buf{ii}(:)];
-		end 
-		source_profile( numel( source_profile )+1 ) = r_sp{1};
-	end
-	
-	%number of surviving runs
-	nb_runZ = numel( source_profile );
+	%merge the data
+	data = cell2mat( data ); %this should do it. No sort or cuts.
 end
 		
--- octave/scripts/cryscalib/cctop_merge_lores.m
+++
@@ -1,8 +0,0 @@
-%this utility will merge "lores" for crystals --each lore is the data necessary
-%for a calibration got from a single source run.
-%
-% calinf = cctop_merge_lores( lore, source_profiles )
-
-function calinf = cctop_merge_lores( lore, source_profiles )
-	%...
-end
