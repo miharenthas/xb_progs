fb19571c37b0f256a4f0ab94ba36243ff2f9bd38
Lorenzo Zanetti <lzanetti@ikp.tu-darmstadt.de>
2017-10-27 16:23:42 +0200

    added a tool to cut on track fields. Seems to be working.

---
+++ octave/msrc/xb_trk_cut_on_field.m
@@ -0,0 +1,93 @@
+%this function selects hits based on the content of a field.
+%usage: [evt, nb_removed] = xb_trk_cut_on_field( evt, op_handle )
+%       where "op_handle" is a function handle that
+%           -takes an aray of energies as argument
+%           -returns an array of boolean values (or something that can
+%            go into an if statement)
+%       NOTE: those for which TRUE is returned are KEPT!
+
+function [evt, nb_removed] = xb_trk_cut_on_field( evt, op_handle, field_name )
+	if ~is_function_handle( op_handle )
+		error( "Second argument **MUST** be a function handle!" );
+	end
+	if ~ischar( field_name )
+		error( 'Third argument **MUST** be a string.' );
+	end
+	if ~isfield( evt, field_name )
+		warning( ['No field named "',field_name,'" in the given structure.'] );
+		nb_removed = 0;
+		return;
+	end
+	if isempty( evt )
+		nb_removed = 0;
+		return;
+	end
+	
+	%prepare to spli the events in the number of processes
+	nb_events = length( evt );
+	adj = mod( nb_events, nproc );
+	if adj adj = nproc - adj; end
+	idx_part = 1:(nb_events+adj)/nproc:(nb_events+adj);
+	idx_part = linspace( idx_part, ...
+	                     idx_part+(nb_events+adj)/nproc-1, ...
+	                     (nb_events+adj)/nproc );
+	
+	%split the events into a cell
+	evt_part = {};
+	evt_rest = [];
+	nbr_rest = 0;
+	nb_proc = 1;
+	for ii=1:nproc
+		try
+			evt_part(ii) = evt(idx_part(ii,:));
+		catch
+			%I'm not yet sure this is the most brilliant solution
+			%but it's the best I can think right now.
+			[evt_rest, nbr_rest] = xb_trk_cut_on_field( evt(idx_part(ii):end), ...
+			                                             op_handle, field_name );
+			break;
+		end
+		nb_proc += 1;
+	end
+
+	%do the parallel execution
+	proc_handle = @( p ) _processor( p, op_handle, field_name );
+	try
+		pkg load parallel;
+		[evt_part, nb_removed_part] = parcellfun( nb_proc, proc_handle, ...
+		                                          evt_part, 'VerboseLevel', 0 );
+	catch
+		warning( 'Parallel package not available. This will take a while.' );
+		[evt_part, nb_removed_part] = cellfun( proc_handle, evt_part );
+	end
+	
+	%stitch together the stuff
+	evt = reshape( evt_part, [], 1 );
+	if ~isempty( evt_rest ) evt = [evt(:); evt_rest(:)]; end
+	nb_removed = sum( nb_removed_part ) + nbr_rest; 
+
+	%prune the empty ones
+	evt = evt( find( [evt.n] ) );
+end
+
+%processor function: does the actaul cutting.
+%it's called in parallel... for a bit of speed.
+function [evt, nb_removed] = _processor( evt, op_handle, field_name )
+	%loop clear the uninsteresting stuff.
+	nb_removed = sum( [evt.n] );
+	for ii=1:length( evt )
+		keep_idx = find( op_handle( [evt(ii).( field_name )](:) ) );
+		if size( evt(ii).fragment_A ) evt(ii).fragment_A = evt(ii).fragment_A( keep_idx ); end
+		if size( evt(ii).fragment_Z ) evt(ii).fragment_Z = evt(ii).fragment_Z( keep_idx ); end
+		if size( evt(ii).incoming ) evt(ii).incoming = evt(ii).incoming( keep_idx ); end
+		if size( evt(ii).outgoing ) evt(ii).outgoing = evt(ii).outgoing( keep_idx ); end
+		if size( evt(ii).fragment_beta )
+			evt(ii).fragment_beta = evt(ii).fragment_beta( keep_idx );
+		end
+
+		%at the end of things, multiplicity update
+		evt(ii).n = length( keep_idx );
+	end
+
+	nb_removed = nb_removed - sum( [evt.n] );
+end
