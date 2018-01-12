a2651c16a91a4e761b5ba072f2096c2e42fe6704
Morningrize <ahorvat@ikp.tu-darmstadt.de>
2018-01-12 14:08:14 +0100

    screwup with previous commit

--- include/xb_root_writer.h
+++ include/xb_root_writer.h
@@ -4,8 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
+#include <algorithm>
 #include "TFile.h"
 #include "TTree.h"
+#include "TChain.h"
+#include "TKey.h"
+#include "TObject.h"
+#include "TClonesArray.h"
 
 #include "RVersion.h"
 #if ROOT_VERSION_CODE >= ROOT_VERSION( 5, 99, 99 )
@@ -14,9 +19,12 @@
 
 #include "xb_cluster.h"
 
+
 namespace XB{
         //writing rootfile for cluster data
 	void rwrite( char* f_root_out, std::vector<XB::clusterZ> &event_klZ);
+	//write and stitch 
+	void rwrite( char* f_root_out, char* stch_r_file, std::vector<XB::clusterZ> &event_klZ, bool v_flag=false);
 }
 
 #endif
