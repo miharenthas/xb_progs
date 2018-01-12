4e5581a56c0e2d09562150da23f9c24a784ed91b
Morningrize <ahorvat@ikp.tu-darmstadt.de>
2018-01-12 14:08:14 +0100

    screwup with previous commit

--- src/xb_converter.cpp
+++ src/xb_converter.cpp
@@ -14,13 +14,13 @@
 using namespace std;
 
 int main( int argc, char **argv ){
-	char in_bin_name[256], out_root_name[256];
-	bool in_flag = false, out_flag = false, verbose = false;	
+	char in_bin_name[256], out_root_name[256], stitch_f_name[256];
+	bool in_flag = false, out_flag = false, verbose = false, stitch=false;	
 
 	//get ONE filename from the command line.
 	if (argc<2) {
 		puts("No options provided, I will die now since I have no purpose");
-		return 1;
+		exit( 1 );
 	}
 
         if( argv[1][0] != '-' && strlen( argv[1] ) < 256 ){
@@ -29,7 +29,7 @@ int main( int argc, char **argv ){
         }
 	//input parsing
 	char iota = 0;
-        while( (iota = getopt( argc, argv, "i:o:v") ) != -1 )
+        while( (iota = getopt( argc, argv, "i:o:s:v") ) != -1 )
                 switch( iota ){
                         case 'i' : //set an input file
                                 if( strlen( optarg ) < 256 ){
@@ -43,33 +43,45 @@ int main( int argc, char **argv ){
                                         out_flag = true;
                                 }
                                 break;
+                        case 's' : //set an output file
+                                if( strlen( optarg ) < 256 ){
+                                        strcpy( stitch_f_name, optarg );
+                                        stitch = true;
+                                }
+                                break;
+
                         case 'v' : //be verbose
                                 verbose = true;
                                 break;
 	                default:
                                 printf( "-%c is not a valid option.\n", optopt );
-                                printf( "usage: xb_converter [-i[FILE]|-o[FILE]|-v]\n" );
+                                printf( "usage: xb_converter [-i[FILE]|-o[FILE]|-s[FILE]|-v]\n" );
                                 printf( "if -i isn't specified, stdin is used\n" );
                                 exit( 1 );
                 }
 	if( !out_flag ) {
-		printf( "Please give me a name for the output\n" );
-		return 1;
+		printf( "Please give me a name for the output root file \n" );
+		exit( 1 );
 	}
+
 	//hello
 	if( verbose ) printf( "*** I will try to make you a rootfile ***\n" );
 	if( verbose && in_flag ) printf( "Reading from: %s...\n", in_bin_name );
 	else if( verbose && !in_flag ) printf( "Reading from STDIN...\n" );
 
-	//read in clustered data
+	//read in cluster data
 	std::vector<XB::clusterZ> klz;
 	if( in_flag ) XB::load( in_bin_name, klz );
 	else XB::load( stdin, klz );
 
-	//write a rootfile
-	if( verbose ) printf( "Writing file: %s \n",out_root_name);
-	XB::rwrite( out_root_name, klz );
-
-
+	if ( verbose ) printf( "Writing file: %s \n",out_root_name);
+	//write a rootfile, no stitching
+	if ( !stitch ) {		
+		XB::rwrite( out_root_name, klz);
+	}
+	else {
+		if ( verbose ) printf( "Stitching to root file: %s \n",stitch_f_name );
+		XB::rwrite( out_root_name, stitch_f_name, klz, verbose );
+	}
 return 0;
 }
--- src/xb_root_writer.cc
+++ src/xb_root_writer.cc
@@ -2,12 +2,16 @@
 
 #include "xb_root_writer.h"
 
+//this one just writes an XBc rootfile
 void XB::rwrite(char* f_root_out, std::vector<XB::clusterZ> &event_klZ ){
-
+	//event
 	unsigned int Xbevnt=0;
 	unsigned int Xbcmult=0;
-	unsigned int Xbcn[160],Xbci[160];
-	float Xbcalt[160],Xbcaz[160],Xbsume[160];
+	//clusters in event
+	unsigned int Xbcn[162],Xbci[162];
+	float Xbcalt[162],Xbcaz[162],Xbcsume[162];
+	//crystals in cluster
+	unsigned int Xbcii[162][162];
 
 	TFile *fout = TFile::Open(f_root_out, "recreate"); //output file
 	TTree *hxbc = new TTree("hxbc", "An XB_cluster TTree"); //the XB cluster tree
@@ -17,20 +21,24 @@ void XB::rwrite(char* f_root_out, std::vector<XB::clusterZ> &event_klZ ){
 	hxbc->Branch("Xbcn",&Xbcn,"Xbcn[Xbcmult]/i"); //number of crystals in a cluster
 	hxbc->Branch("Xbci",&Xbci,"Xbci[Xbcmult]/i"); //index of the centroid
 	hxbc->Branch("Xbcalt",&Xbcalt,"Xbcalt[Xbcmult]/F"); //centroid altitude
-	hxbc->Branch("Xbcaz",&Xbcaz,"Xbcaz/F"); //centroid azimuth
-	hxbc->Branch("Xbsume",&Xbsume,"Xbsume/F"); //gamma sum energy of cluster
-	
+	hxbc->Branch("Xbcaz",&Xbcaz,"Xbcaz[Xbcmult]/F"); //centroid azimuth
+	hxbc->Branch("Xbcsume",&Xbcsume,"Xbcsume[Xbcmult]/F"); //gamma sum energy of cluster 
+	hxbc->Branch("Xbcii",&Xbcii,"Xbcii[Xbcmult][162]/i");//indices of paricipating crystals, organised clusterwise
+
 	//start eventloop
-	for( int i=0; i < event_klZ.size(); ++i ){
+	for( unsigned int i=0; i < event_klZ.size(); ++i ){
 		Xbevnt=event_klZ[i].evnt;	
 		Xbcmult=event_klZ[i].n;
 		if (Xbcmult>0){ //are there any clusters?
-			for( int k=0; k < event_klZ[i].n; ++k ){//loop over the clusters
+			for ( unsigned int k=0; k < event_klZ[i].n; ++k ){//loop over the clusters
 				Xbcn[k]=event_klZ[i].clusters[k].n;
 				Xbci[k]=event_klZ[i].clusters[k].centroid_id;
 				Xbcalt[k]=event_klZ[i].clusters[k].c_altitude;
 				Xbcaz[k]=event_klZ[i].clusters[k].c_azimuth;
-				Xbsume[k]=event_klZ[i].clusters[k].sum_e;
+				Xbcsume[k]=event_klZ[i].clusters[k].sum_e;
+				for ( unsigned int cr=0; cr < event_klZ[i].clusters[k].n; cr++ ){//loop over crystals in cluster
+					Xbcii[k][cr]=event_klZ[i].clusters[k].crys.at(cr);
+				} 
 			}
 		}
 	hxbc->Fill();
@@ -38,4 +46,112 @@ void XB::rwrite(char* f_root_out, std::vector<XB::clusterZ> &event_klZ ){
 	hxbc->Write();
 	fout->Close();	
 }
+
+// this one will stitch
+void XB::rwrite(char* f_root_out, char* stch_r_file, std::vector<XB::clusterZ> &event_klZ, bool v_flag){
+
+        unsigned int Xbcmult=0;
+        unsigned int Xbcn[162],Xbci[162];
+        float Xbcalt[162],Xbcaz[162],Xbcsume[162];
+	//crystals in cluster
+	unsigned int Xbcii[162][162];
+
+	TFile stitch_file( stch_r_file );
+	if( stitch_file.IsZombie() ){
+                throw XB::error( "File error!", "XB::rwrite" );
+        }
+
+	//relating to the root file we stitch the XB data to
+	//get the name of the TTree
+	TIter nextkey( stitch_file.GetListOfKeys() );
+        TKey *key;
+        TObject *obj;
+        TTree *data_tree;
+        while( (key = (TKey*)nextkey()) ){
+                obj = key->ReadObj();
+                if( obj->IsA()->InheritsFrom( TTree::Class() ) ){
+                        data_tree = (TTree*)obj;
+                        break;
+                }
+        }
+
+	//we need the event id from the tree
+	int Evnt;
+	TBranch *b_Evnt;
+
+	data_tree->SetBranchAddress("Evnt",&Evnt, &b_Evnt);	
+
+	//clone the tree into a new root file	
+	TFile* fout = new TFile( f_root_out, "recreate" );
+	TTree *newtr = data_tree->CloneTree();
+	//add new branches
+        TBranch *bxbcmult = newtr->Branch("Xbcmult",&Xbcmult); //number of clusters in an event
+        TBranch *bxbcn = newtr->Branch("Xbcn",&Xbcn,"Xbcn[Xbcmult]/i"); //number of crystals in a cluster
+        TBranch *bxbci = newtr->Branch("Xbci",&Xbci,"Xbci[Xbcmult]/i"); //index of the centroid
+        TBranch *bxbcalt = newtr->Branch("Xbcalt",&Xbcalt,"Xbcalt[Xbcmult]/F"); //centroid altitude
+        TBranch *bxbcaz = newtr->Branch("Xbcaz",&Xbcaz,"Xbcaz[Xbcmult]/F"); //centroid azimuth
+        TBranch *bxbcsume = newtr->Branch("Xbcsume",&Xbcsume,"Xbcsume[Xbcmult]/F"); //gamma sum energy of cluster
+	TBranch *bxbcii = newtr->Branch("Xbcii",&Xbcii,"Xbcii[Xbcmult][162]/i");//indices of paricipating crystals, organised clusterwise
+
+	//guess the ordering
+	bool st_lth = true, cl_lth = true;
+	if ( event_klZ[1].evnt > event_klZ[2].evnt ) cl_lth = false;	
+	newtr->GetEvent(1);unsigned int ev1=Evnt;
+	newtr->GetEvent(2);unsigned int ev2=Evnt;	
+	if ( ev1 > ev2 ) st_lth = false;	
+
+	//iterator
+	unsigned int cl_i = event_klZ.size() - 1; 
+	if ( st_lth && cl_lth ) cl_i = 0; 
+	unsigned int v_cl_i=cl_i;
+
+	//loop over events 
+	for (int jentry=0;jentry<(int)newtr->GetEntries();jentry++){
+		newtr->GetEvent(jentry);	
+		if ( v_flag && (float)(jentry/25000.)==(int)(jentry/25000.) ) printf( "%i %% done \n", (int)(100.*jentry/((int)newtr->GetEntries())) );  
+		bool match = false;
+		//find the match in the cluster data
+		while ( !match && cl_i >= 0 && cl_i < event_klZ.size() ) {
+			//fix the screw up in event id uint/int between rootfile and xb data
+			unsigned int tmp_id;
+			memcpy( &tmp_id, &Evnt, sizeof(unsigned int));
+			if ( st_lth && cl_lth ) { //same ordering
+				if ( event_klZ[cl_i].evnt == tmp_id  ) match = true;
+				else cl_i++;
+			}
+			else {	//opposite ordering
+				if (  event_klZ[cl_i].evnt == tmp_id ) match = true;
+                                else cl_i--;
+			}
+		}//end of search over cluster events
+		//reset counter to last entry found
+		if (match) v_cl_i=cl_i;
+		else {cl_i=v_cl_i;continue;}
+                Xbcmult=event_klZ[cl_i].n;
+                if (Xbcmult>0){ //are there any clusters?
+                        for( unsigned int k=0; k < event_klZ[cl_i].n; ++k ){//loop over the clusters
+                                Xbcn[k]=event_klZ[cl_i].clusters[k].n;
+                                Xbci[k]=event_klZ[cl_i].clusters[k].centroid_id;
+                                Xbcalt[k]=event_klZ[cl_i].clusters[k].c_altitude;
+                                Xbcaz[k]=event_klZ[cl_i].clusters[k].c_azimuth;
+                                Xbcsume[k]=event_klZ[cl_i].clusters[k].sum_e;
+				for ( unsigned int cr=0; cr < event_klZ[cl_i].clusters[k].n; cr++ ){//loop over all crystals in cluster
+					Xbcii[k][cr]=event_klZ[cl_i].clusters[k].crys.at(cr);
+				} 
+                        }
+                }
+		//Fill the new branches
+		bxbcmult->Fill();
+		bxbcn->Fill();
+		bxbci->Fill();
+		bxbcalt->Fill();
+		bxbcaz->Fill();
+		bxbcsume->Fill();
+		bxbcii->Fill();
+	}//end of eventloop (stitch file)
+	newtr->Write();
+	delete newtr;
+	fout->Close();
+}
+
 
